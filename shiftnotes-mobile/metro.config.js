0de3a293a8b6b82ff135d531c8418d5f4041a6b5
Shomari Ewing <shomari.ewing@gmail.com>
2025-10-20 16:40:17 -0500

    Add explicit path resolution config for EAS builds

--- shiftnotes-mobile/metro.config.js
+++ shiftnotes-mobile/metro.config.js
@@ -1,6 +1,11 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+// Set explicit project root
+config.projectRoot = __dirname;
+config.watchFolders = [__dirname];
 
 // Add CSS support for web builds
 config.resolver.assetExts.push('css');
@@ -10,5 +15,10 @@ config.transformer.assetPlugins = ['expo-asset/tools/hashAssetFiles'];
 
 // Handle CSS imports properly
 config.resolver.platforms = ['native', 'web', 'ios', 'android'];
+
+// Ensure node_modules resolution
+config.resolver.nodeModulesPaths = [
+  path.resolve(__dirname, 'node_modules'),
+];
 
 module.exports = config;
